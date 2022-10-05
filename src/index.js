923e5c03d33624383f3d2dae4f909a95964d4678
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-10-05 16:40:47 +0500

    05-10-2022

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
     <StrictMode>
         <ReduxProvider store={store}>
-            <BrowserRouter basename="/">
+            <BrowserRouter basename="/mis">
                 <App />
             </BrowserRouter>
         </ReduxProvider>
