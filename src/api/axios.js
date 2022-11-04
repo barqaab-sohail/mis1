38bd62a10568fa9473c328aa62b54605cb3421d5
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2022-11-04 16:24:30 +0500

    04-11-2022

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const token = localStorage.getItem('auth_token');
 export default axios.create({
     withCredentials: true,
-    baseURL: 'https://barqaab.pk/hrms/public/api/',
-    //'http://192.168.1.10/hrms/public/api/',
+    baseURL:
+        //'https://barqaab.pk/hrms/public/api/',
+        'http://192.168.1.10/hrms/public/api/',
     //'http://192.168.0.121/hrms/public/api/',
     //'http://192.168.1.8/hrms/public/api/',
 
